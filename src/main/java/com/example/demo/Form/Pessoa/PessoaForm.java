2b825b6ccd29a893ff34bf93cc7e7a4962ad3842
rafael_o_mattos <rafarafosomattos@gmail.com>
2024-08-16 21:54:18 -0300

    ProjetoPCD16_08

--- src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
+++ src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -58,6 +59,33 @@ public class PessoaForm {
     }
     public void setListDeficiencia(DeficienciaRepository repository){
         this.listDeficiencia = repository.findAll();
+    }
 
+   
+    @NotBlank(message = "Preencha o CEP")
+    @Size(min = 9, max = 9, message = "Preeencha com um CEP válido")
+    private String cep;
+
+    @NotNull(message = "Preencha o campo")
+    private String uf;
+
+    @NotNull(message = "Preencha o campo")
+    private String cidade;
+
+    @NotNull(message = "Preencha o campo")
+    private String bairro;
+
+    @NotNull(message = "Preencha o campo")
+    private String logradouro;
+
+    @NotNull(message = "Preencha o campo")
+    private String numero;
+
+    @NotNull(message = "Preencha o campo")
+    private String complemento;
 }
-}
+
+
+    
+    
+
